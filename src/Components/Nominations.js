fb9da9c010dce0dc3b8c7303b4f1017eab41e2c9
Akif Manzoor <akifmanzoor124@gmail.com>
2021-01-15 21:08:28 -0500

    cleaned up Nominations.js

--- src/Components/Nominations.js
+++ src/Components/Nominations.js
@@ -5,7 +5,10 @@ function Nominations({ nominations, removeNomination }) {
         <div>
             <h3>Nominations</h3>
             <ul>
-                {nominations.map(nomination => <li key={nomination.imdbID}>{nomination.Title} {nomination.date} <button onClick={() => removeNomination(nomination)} >Remove</button></li>)}
+                {nominations.map(nomination =>
+                    <li key={nomination.imdbID}>{nomination.Title} {nomination.date}
+                        <button onClick={() => removeNomination(nomination)} >Remove</button>
+                    </li>)}
             </ul>
         </div>
     )
